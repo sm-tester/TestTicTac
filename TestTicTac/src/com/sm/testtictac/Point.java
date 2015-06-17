76fa71779f80c78a5e176c8c385dc8028b7edcb8
SMA <dark_27@list.ru>
2015-06-17 16:31:16 +0500

    *

--- TestTicTac/src/com/sm/testtictac/Point.java
+++ TestTicTac/src/com/sm/testtictac/Point.java
@@ -42,9 +42,3 @@ public class Point
     return "(" + this.mX + ", " + this.mY + ")";
   }
 }
-
-
-/* Location:              /Users/admin/Downloads/Hacking/Five in a row_1.2-dex2jar.jar!/com/games/fiveinarow/models/Point.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1-SNAPSHOT-20140817
- */
